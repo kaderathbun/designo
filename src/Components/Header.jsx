f73cb31695e4e3acd955156336fb7be990106e19
Kade <106041496+kaderathbun@users.noreply.github.com>
2022-11-22 22:20:17 -0500

    002 - mobile nav style and logic

---
+++ src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+//styles
+import './Header.scss'
+import logo from '../assets/shared/desktop/logo-dark.png'
+import navHamburgerIcon from '../assets/shared/mobile/icon-hamburger.svg'
+import navCloseIcon from '../assets/shared/mobile/icon-close.svg'
+
+export default function Header() {
+  //mobile logic
+  const [isOpen, setIsOpen] = useState(false)
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img src={logo} alt="Designo" className="header__logo" />
+        <img
+          src={isOpen ? navCloseIcon : navHamburgerIcon}
+          alt=""
+          className="header__hamburger-icon"
+          onClick={handleClick}
+        />
+      </div>
+      <ul
+        className={
+          isOpen ? 'header__menu header__menu--active' : 'header__menu'
+        }
+      >
+        <Link className="header__link">Our Company</Link>
+        <Link className="header__link">Locations</Link>
+        <Link className="header__link">Contact</Link>
+      </ul>
+    </header>
+  )
+}
