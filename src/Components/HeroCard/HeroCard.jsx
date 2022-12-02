c1413de82bbc8a7896762478b45d511bf68c1c39
Kade <106041496+kaderathbun@users.noreply.github.com>
2022-12-02 14:43:00 -0500

    019 - design product pages tablet styles

--- src/Components/HeroCard/HeroCard.jsx
+++ src/Components/HeroCard/HeroCard.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 //styles
 import './HeroCard.scss'
+import backgroundMobile from '../../assets/home/desktop/bg-pattern-hero-home.svg'
+import backgroundDesktop from '../../assets/shared/desktop/bg-pattern-call-to-action.svg'
 
 export default function HeroCard({ heading, subHeading, isSecondary }) {
-  return (
-    <div
-      className="hero__card"
-      style={{
-        height: isSecondary ? '395px' : '320px',
-        backgroundSize: isSecondary ? '70%' : 'contain',
-        backgroundPosition: isSecondary ? 'top -110px right' : 'top right',
-        backgroundRepeat: isSecondary ? 'initial' : 'no-repeat',
-        backgroundRepeatY: isSecondary ? 'no-repeat' : 'initial',
-      }}
-    >
-      <h1 className="hero__heading">{heading}</h1>
-      <p className="hero__sub-heading">{subHeading}</p>
-    </div>
-  )
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
+  const isTablet = useMediaQuery({ query: '(min-width: 768px)' })
+  const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' })
+
+  const stylesMobile = {
+    height: isSecondary ? '395px' : '320px',
+    backgroundImage: 'url(' + backgroundMobile + ')',
+    backgroundSize: isSecondary ? '70%' : 'contain',
+    backgroundPosition: isSecondary ? 'top -110px right' : 'top right',
+    backgroundRepeat: isSecondary ? 'initial' : 'no-repeat',
+    backgroundRepeatY: isSecondary ? 'no-repeat' : 'initial',
+  }
+  const stylesTablet = {
+    height: isSecondary ? '395px' : '252px',
+    backgroundImage: 'url(' + backgroundDesktop + ')',
+    backgroundSize: isSecondary ? '70%' : 'auto',
+    backgroundPosition: isSecondary ? 'top -110px right' : 'center',
+    backgroundRepeatY: isSecondary ? 'no-repeat' : 'initial',
+  }
+  const stylesDesktop = {
+    height: isSecondary ? '395px' : '320px',
+    backgroundImage: 'url(' + backgroundDesktop + ')',
+    backgroundSize: isSecondary ? '70%' : 'contain',
+    backgroundPosition: isSecondary ? 'top -110px right' : 'top right',
+    backgroundRepeatY: isSecondary ? 'no-repeat' : 'initial',
+  }
+
+  if (isMobile) {
+    return (
+      <div className="hero__card" style={stylesMobile}>
+        <h1 className="hero__heading">{heading}</h1>
+        <p className="hero__sub-heading">{subHeading}</p>
+      </div>
+    )
+  } else if (isTablet) {
+    return (
+      <div className="hero__card" style={stylesTablet}>
+        <h1 className="hero__heading">{heading}</h1>
+        <p className="hero__sub-heading">{subHeading}</p>
+      </div>
+    )
+  } else if (isDesktop) {
+    return (
+      <div className="hero__card" style={stylesDesktop}>
+        <h1 className="hero__heading">{heading}</h1>
+        <p className="hero__sub-heading">{subHeading}</p>
+      </div>
+    )
+  }
 }
